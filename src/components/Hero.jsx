26ee7e1e919f276e9dc4e4fadf0af32d4bb3313e
rshshetty <65729666+rshshetty@users.noreply.github.com>
2021-11-26 13:05:20 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
-//import heroBcg2 from "../assets/hero-bcg-2.jpeg";
+
 const Hero = () => {
   return (
     <Wrapper className='section-center'>
